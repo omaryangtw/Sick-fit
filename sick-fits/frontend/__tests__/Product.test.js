d597a0234169bc5088aa41b88e12ff71c5d14a21
A-J Roos <asjas@hey.com>
2021-12-06 14:05:32 +0200

    fix tests by adding the CartStateProvider

--- sick-fits/frontend/__tests__/Product.test.js
+++ sick-fits/frontend/__tests__/Product.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
+import { CartStateProvider } from '../lib/cartState';
 
 import Product from '../components/Product';
 import { fakeItem } from '../lib/testUtils';
@@ -10,9 +11,11 @@ const product = fakeItem();
 describe('<Product/>', () => {
   it('renders out the price tag and title', () => {
     const { container, debug } = render(
-      <MockedProvider>
-        <Product product={product} />
-      </MockedProvider>
+      <CartStateProvider>
+        <MockedProvider>
+          <Product product={product} />
+        </MockedProvider>
+      </CartStateProvider>
     );
     debug();
   });
--- sick-fits/frontend/jest.setup.js
+++ sick-fits/frontend/jest.setup.js
@@ -1,7 +1,3 @@
 import '@testing-library/jest-dom';
 
-import React from 'react';
-
 window.alert = console.log;
-
-global.React = React; // this also works for other globally available libraries
